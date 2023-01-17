be2d28fe89086aef46a3e92fac1eaf772a2e2e10
unknown <sarasaravevic@gmail.com>
2023-01-17 18:25:47 +0100

    adding handler functions

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,17 +1,30 @@
 import "./AddUser.css";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import { useState } from "react";
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const addUserHandler = (event) => {
     event.preventDefault();
+  };
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
   return (
     <Card className={"input"}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserInput</label>
-        <input id="username" type="text"></input>
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+        ></input>
         <label htmlFor="age">Age</label>
-        <input id="age" type="number"></input>
+        <input id="age" type="number" onChange={ageChangeHandler}></input>
         <Button className="button" type="submit">
           Add User
         </Button>
